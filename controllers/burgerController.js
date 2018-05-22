aef21f7a2ec021c14d2650e3e087c85580e44122
joglatt <34425753+joglatt@users.noreply.github.com>
2018-05-22 17:51:08 -0400

    incremental

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -3,3 +3,12 @@ var express = require("express");
 var router = express.Router();
 
 var burger = require("../models/burger.js");
+
+router.get('/', function(req,res){
+    burger.selectAll(function(data){
+        var object1 ={
+            bugers:data
+        };
+        console.log(object1);
+    })
+})
